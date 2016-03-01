ca2a9c9604d01be2adbb7ee0a726aa57d2ec0118
begdor <dulikuhunst@gmail.com>
2016-02-29 16:11:20 -0800

    test Promise

--- login/test/myTestUser.js
+++ login/test/myTestUser.js
@@ -3,14 +3,30 @@
 var User = require('./../models/User');
 var mongoose = require('mongoose');
 var chai = require('chai');
-var expect = chai.expect;
+var expect = require('chai').expect;
 
+//SYNTAX: describe (moduleName, testDetails)
 describe('.getByUsername(username)', function () {
-	before(function(done){
-		this.user = new User();
-		done();
-	});
+
 	it('should be a function', function () {
 		expect(User.getByUsername).to.be.a('function');
+	});
+
+	before(function(done){
+		this.user = new User({username:'likun',password:'nonsense'});
+		done();
+	});
+	//Working with Promise: return Promise instead of done
+	it.only('resolve user if found', function (done) {
+		var self = this;
+		
+		return User.getByUsername(this.user.username)
+		.then(function (user) {
+			expect(user).to.exist;
+			expect(user.username).to.equal(self.user.username);
+		},
+		function(err){
+			done(err);
+		});
 	});
 });
